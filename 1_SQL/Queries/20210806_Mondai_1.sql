5c0084009cb67fb69e90918c09269039e991ae88
Kesavan Nanjappan <k7nanjappan@gmail.com>
2021-08-10 09:07:49 +0530

    Fixed Date format

--- 1_SQL/Queries/20210806_Mondai_1.sql
+++ 1_SQL/Queries/20210806_Mondai_1.sql
@@ -1,6 +1,6 @@
 SELECT member_cd AS 会員CD, 
 gender_kbn AS 性別区分, 
 family_kj AS 名前, 
-to_char(date(birthday_ts), 'DD/MM/YYYY') AS 誕生日 
+to_char(date(birthday_ts), 'YYYY/MM/DD') AS 誕生日 
 FROM t_member 
 WHERE member_id IN (2, 3, 4);
