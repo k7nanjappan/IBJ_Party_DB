b8016377fb6d10149d522a38166f9f3709c4599b
Kesavan Nanjappan <k7nanjappan@gmail.com>
2021-08-10 09:10:23 +0530

    Fixed Date format

--- 1_SQL/Queries/20210806_Mondai_3.sql
+++ 1_SQL/Queries/20210806_Mondai_3.sql
@@ -1,6 +1,6 @@
 SELECT t_party.party_id AS パーティーID, 
 t_party.title AS パーティー名,  
-to_char(t_party.start_ts, 'DD/MM/YYYY hh24:mm') AS 開催日時
+to_char(t_party.start_ts, 'YYYY/MM/DD hh24:mm') AS 開催日時
 FROM t_party
 WHERE (t_party.party_id) IN
 	(SELECT t_party_member.party_id FROM t_party_member WHERE t_party.party_id = t_party_member.party_id AND
